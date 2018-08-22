f61ff4e21bd96a847439c28c7cdd4e0bb6e3c912
Ri <Ri@Profs-MacBook-Air.local>
2018-08-22 14:25:42 -0400

    Analysing debugging behaviour dataset

---
+++ debugging-behaviour.py
@@ -0,0 +1,78 @@
+############################################################
+# This code is to analyze the debugging-behaviour.csv file #
+############################################################
+
+import pandas as pd 
+
+### Reading .csv files
+debugging_behaviour = pd.read_csv("/Summer 2018/Dr. Cliff Shaffer's Lab/DSP vs Launch Times Analysis/debugging-behaviour.csv")
+
+### Storing the values under DataFrame
+df = pd.DataFrame(debugging_behaviour)
+
+### Calculating total number of projects
+# print "Total rows: " + str(len(df))
+
+### Creating a unique list
+unique_list = df.userName.unique()
+
+### Calculating the total number of students
+#print len(unique_list) # 176 unique students in the course
+
+df_debugger = df[df.debugSessionCount != 0]
+
+### Calculating projects that launch debugger at least once
+# print len(df_debugger) # 406 projects have debugger launches
+
+debugger_list = df_debugger.userName.unique()
+
+### Calculating the students who use debugger at least once
+# print len(debugger_list) # 147 students use debugger in at least one of their projects
+
+### Dropping the debugSessionCount column
+df_prnt_stmt = df.drop(columns=['debugSessionCount'])
+
+# print df_prnt_stmt
+
+### Adding both of the launches
+df_prnt_stmt['totalLaunches'] = df_prnt_stmt['normalLaunches'] + df_prnt_stmt['testLaunches']
+
+# print df_prnt_stmt
+
+### Keeping only the rows with non-zero totalLaunches
+df_prnt_stmt_nonzero = df_prnt_stmt[df_prnt_stmt.totalLaunches != 0]
+
+### Calculating projects with at least one DPS
+# print len(df_prnt_stmt_nonzero) # 224 projects have atleast a DPS in either a normalLaunch or a testLaunch 
+
+prnt_stmt_list = df_prnt_stmt_nonzero.userName.unique()
+
+# print len(prnt_stmt_list) # 126 students used DPS at least once (either in normalLaunch or testLaunch)
+
+prnt_stmt_normal = df_prnt_stmt[df_prnt_stmt.normalLaunches != 0]
+prnt_stmt_test = df_prnt_stmt[df_prnt_stmt.testLaunches != 0]
+
+# print len(prnt_stmt_normal) # 164 projects have atleast one DPS launch in normal launches 
+# print len(prnt_stmt_test) # 217 projects have atleast one DPS launch in test launches 
+
+prnt_stmt_both = prnt_stmt_normal[prnt_stmt_normal.testLaunches != 0]
+
+# print len(prnt_stmt_both) # 157 projects have atleast one DPS launch in both normal launches and test launches
+
+### Calculating number of submissions for each projects 
+df_p1 = df[df.assignment == 'Project 4']
+
+print len(df_p1) # 164 submissions for Project 1
+# 144 submissions for Project 2 
+# 134 submissions for Project 3
+# 125 submissions for Project 4
+# 164 + 144 + 134 + 125 = 567 (total)
+
+
+
+
+# df_prnt_stmt = df[df.normalLaunches != 0 or df.testLaunches != 0] # ValueError
+
+# prnt_stmt_list = df_prnt_stmt.userName.unique()
+
+# print len(prnt_stmt_list)
--- dsp_calc.py
+++ dsp_calc.py
@@ -10,5 +10,27 @@ concat = pd.read_csv("/Summer 2018/Dr. Cliff Shaffer's Lab/DSP vs Launch Times A
 ### Storing the values under DataFrame
 df_concat = pd.DataFrame(concat)
 
+def count_printy_launches(usergroup):
+	count_list = []
+	# count_list.append('DSPCount')
+	### Looping through the data
+	count = 0
+	for index, row in usergroup.iterrows():
+		if row['Subtype'] == 'Temporary':
+			if row['Subsubtype'] == 'Insertion':
+				count = count + 1
+			elif row['Subsubtype'] == 'Deletion':
+				count = count - 1
+		# print row['PrintStatements'], row['Subsubtype']
+		count_list.append(count)
+
+	series = pd.Series(count_list)
+	usergroup['DSPCount'] = series.values
+	return usergroup
+
+results = df_concat.groupby(['userName', 'assignment']).apply(count_printy_launches)
+
 ### Output the values in .csv file
-df_concat.to_csv("/Summer 2018/Dr. Cliff Shaffer's Lab/DSP vs Launch Times Analysis/df_launches_dsp_calc.csv", index=False)
+results.to_csv("/Summer 2018/Dr. Cliff Shaffer's Lab/DSP vs Launch Times Analysis/df_launches_dsp_calc.csv", index=False)
+
+
