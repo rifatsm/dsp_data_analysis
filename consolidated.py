02d70442784a379105ae514dc3becc8500eb8b87
Ri <Ri@Profs-MacBook-Air.local>
2018-08-23 16:00:44 -0400

    # Developing Cluster correctness for the dataset

--- consolidated.py
+++ consolidated.py
@@ -7,6 +7,27 @@
 from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np 
+
+
+### Calculating Correctness of the Kmeans algo:
+def kmeans_correctness(clusters, X, y):
+	kmeans = KMeans(n_clusters=clusters)
+	kmeans.fit(X)
+
+	correct = 0
+	for i in range(len(X)):
+	    predict_me = np.array(X[i].astype(float))
+	    predict_me = predict_me.reshape(-1, len(predict_me))
+	    prediction = kmeans.predict(predict_me)
+	    if prediction[0] == y[i]:
+	        correct += 1
+
+	# print correct
+	# print len(X)
+
+	return float(correct)/float(len(X))
+
 
 ### Reading from files
 consolidated = pd.read_csv("/Summer 2018/Dr. Cliff Shaffer's Lab/DSP vs Launch Times Analysis/consolidated.csv")
@@ -17,7 +38,29 @@ df = pd.DataFrame(consolidated)
 ### Selecting specific sample size
 df_p1 = df[df.assignment == 'Project 1']
 
-describe = df_p1.describe()
+# describe = df_p1.describe()
+
+# print df_p1.head()
+
+df_p1 = df_p1.drop(['userName', 'assignment', 'submissionTimeRaw', 'dueDateRaw', 'max.score.correctness', 'score.reftest', 'projectStartTime'], axis=1)
+# df_p1.to_csv("/Summer 2018/Dr. Cliff Shaffer's Lab/DSP vs Launch Times Analysis/df_p1.csv", index=False)
+
+score_array = []
+score_threshold = 0.90
+for index, row in df_p1.iterrows():
+	if row['score'] >= score_threshold:
+		score_array.append(1)
+	else:
+		score_array.append(0)
+
+# print score_array
+
+### Setting X and y values
+X = np.array(df_p1.drop(['score'],1).astype(float))
+y = score_array
+
+for clust in range(1,20):
+	print "clust: " + str(clust) + " " + str(kmeans_correctness(clust, X, y)*100) # The best result is for clust == 2 
 
 # print describe
 
